7dcc7141a2203d59c32b3d790456338d49c24fc9
Fabio Gai <fabio.gai@accenture.com>
2015-04-18 15:52:55 +0200

    first commit

---
+++ KicksAdvisor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KicksAdvisor
+//
+//  Created by Gai, Fabio on 18/04/15.
+//  Copyright (c) 2015 Gai, Fabio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
